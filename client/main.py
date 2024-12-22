b29ffdeeaf650191c5cbdc16c3fbe72c4a6726b2
Salman Ayaz <salmanayazz@gmail.com>
2024-12-21 21:53:20 -0800

    setup hardware monitoring for windows

--- client/main.py
+++ client/main.py
@@ -1,33 +1,71 @@
 import asyncio
 from bleak import BleakClient
+import psutil
+from pynvml import (
+    nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetTemperature,
+    nvmlDeviceGetUtilizationRates, nvmlShutdown
+)
 
 BLE_SERVICE_UUID = "12345678-1234-1234-1234-1234567890ab"
 BLE_CHARACTERISTIC_UUID = "87654321-4321-4321-4321-0987654321ba"
+BLE_ADDRESS = "A0:B7:65:05:68:AE"
 
-def getSystemStats():
+
+def get_cpu_temp():
+    try:
+        import wmi 
+        w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+        sensors = w.Sensor()
+        for sensor in sensors:
+            if sensor.SensorType == 'Temperature' and 'CPU' in sensor.Name:
+                return sensor.Value
+    except Exception as e:
+        print(f"Failed to fetch CPU temperature: {e}")
+    return None
+
+
+
+def get_cpu_util():
+    return psutil.cpu_percent(interval=1)
+
+def get_gpu_stats():
+    gpu_temp, gpu_util = None, None
+    try:
+        nvmlInit()
+        handle = nvmlDeviceGetHandleByIndex(0)
+        gpu_temp = nvmlDeviceGetTemperature(handle, 0)
+        gpu_util = nvmlDeviceGetUtilizationRates(handle).gpu
+    except Exception as e:
+        print(f"Failed to fetch GPU stats: {e}")
     
+    return gpu_temp, gpu_util
+
+def get_hardware_data():
+    gpu_temp, gpu_util = get_gpu_stats()
     return {
-        "cpuTemp": 1,
-        "cpuUtil": 2,
-        "gpuTemp": 3,
-        "gpuUtil": 4,
+        "cpuUtil": psutil.cpu_percent(),
+        "cpuTemp": get_cpu_temp(),
+        "gpuUtil": gpu_util,
+        "gpuTemp": gpu_temp,
+        "memoryUtil": psutil.virtual_memory().percent
     }
 
-async def sendData(address):
+async def send_data(address):
+    print(f"Connecting to device: {address}")
     async with BleakClient(address) as client:
-        print(f"Connected to BLE device: {address}")
+        print(f"Connected")
         
         while True:
-            stats = getSystemStats()
-            data = f"{stats['cpuTemp']},{stats['cpuUtil']},{stats['gpuTemp']},{stats['gpuUtil']}"
             
-            await client.write_gatt_char(BLE_CHARACTERISTIC_UUID, data.encode('utf-8'))
-            print(f"Sent: {data.strip()}")
+            data = get_hardware_data()
+            res = f"{data['cpuUtil']},{data['cpuTemp']},{data['gpuUtil']},{data['gpuTemp']},{data['memoryUtil']}"
+            
+            await client.write_gatt_char(BLE_CHARACTERISTIC_UUID, res.encode('utf-8'))
+            print(f"Sent: {res.strip()}")
             await asyncio.sleep(5)
 
 async def main():
-    bleAddress = "..."
-    await sendData(bleAddress)
+    await send_data(BLE_ADDRESS)
 
 if __name__ == "__main__":
     asyncio.run(main())
